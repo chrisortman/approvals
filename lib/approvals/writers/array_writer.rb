d7192f2e626dbd28afad51688859afbe66823d85
Chris Ortman <chris-ortman@uiowa.edu>
2014-10-03 16:24:32 -0500

    Made a test pass :)
    Had to work around lack of with_index

--- lib/approvals/writers/array_writer.rb
+++ lib/approvals/writers/array_writer.rb
@@ -3,9 +3,20 @@ module Approvals
     class ArrayWriter < TextWriter
 
       def format(data)
-        filter(data).map.with_index do |value, i|
-          "[#{i.inspect}] #{value.inspect}\n"
-        end.join
+        filtered = filter(data).map
+        if filtered.respond_to?(:with_index)
+          filter(data).map.with_index do |value, i|
+            "[#{i.inspect}] #{value.inspect}\n"
+          end.join
+        else
+          i = -1
+          s = []
+          filtered.each do |value|
+            i = i + 1
+            s << "[#{i.inspect}] #{value.inspect}\n"
+          end
+          s.join
+        end
       end
 
       def filter data
